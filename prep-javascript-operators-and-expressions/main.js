97949e267d7f326ae7e8a88ea948bc890acb518e
Tiffany Brown <tdbrown_2001@yahoo.com>
2023-10-04 14:43:03 +0000

    Completed prep-javascript-operators-and-expressions exercise.

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,32 @@
+const number1 = 8;
+const number2 = 16;
+const product = number1 * number2;
+console.log(product, typeof product);
+
+const charge = 78;
+const payment = 40;
+const amountRemaining = charge - payment;
+console.log(amountRemaining, typeof amountRemaining);
+
+const tests = 80;
+const asssignments = 75;
+const final = 95;
+const grade = (tests + asssignments + final) / 3;
+console.log(grade, typeof grade);
+
+const firstName = 'Tiffany';
+const lastName = 'Brown';
+const fullName = firstName + ' ' + lastName;
+console.log(fullName, typeof fullName);
+
+const pH = 3;
+const isAcidic = pH < 7;
+console.log(isAcidic, typeof isAcidic);
+
+const numberOfSoldiers = 100;
+const isSparta = numberOfSoldiers === 300;
+console.log(isSparta, typeof isSparta);
+
+let nickname = 'Tiffany Brown';
+nickname = nickname + ' is the GOAT';
+console.log(nickname, typeof nickname);
